7badbb07329276f3455e0d76a02efce966a74e2c
Domen Koder <domen.koder@gmail.com>
2026-01-14 22:27:35 +0100

    feat: curl is working, still have to save download to file

--- src/downloader.cpp
+++ src/downloader.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
 #include <ostream>
 #include <atlas/downloader.h>
+#include <curl/curl.h>
 
 namespace atlas {
-    void downloader(std::string url) {
-        std::cout << "Downloading from url: " << url << std::endl;
-        std::cout << "Finished downloading." << std::endl;
+    void downloader(const std::string& url) {
+        curl_global_init(CURL_GLOBAL_ALL);
+        CURL *curl = curl_easy_init();
+
+        if(curl) {
+            curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+            curl_easy_perform(curl);
+        }
+
     }
 }

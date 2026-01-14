25f8957cd304cf6667c7a86ef315c45639e22515
Domen Koder <domen.koder@gmail.com>
2026-01-14 20:34:07 +0100

    feat: add the parser (#4)

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include <atlas/parser.h>
 
-int main(int argc, char* argv[]) {
-    std::cout << "Welcome to Atlas!";
+int main(const int argc, char **argv) {
+    std::cout << "Welcome to Atlas!" << std::endl;
+
+    atlas::parser(argc, argv);
+
     return 0;
 }
---
+++ src/parser.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <ostream>
+#include <atlas/parser.h>
+
+namespace atlas {
+    void parser(const int argc, char *argv[]) {
+        if (argc == 1)
+            std::cout << "You must provide at least one argument." << std::endl;
+        else {
+            const std::string url = argv[1];
+            std::cout << url << std::endl;
+        }
+    }
+}

1d51e0409d10da9d6fdf20e1b251e90d2acf952e
Domen Koder <domen.koder@gmail.com>
2026-01-14 20:58:02 +0100

    refactor: added proper exceptions

--- src/parser.cpp
+++ src/parser.cpp
@@ -3,12 +3,9 @@
 #include <atlas/parser.h>
 
 namespace atlas {
+    //argc will be used later, do not delete!
     void parser(const int argc, char *argv[]) {
-        if (argc == 1)
-            std::cout << "You must provide at least one argument." << std::endl;
-        else {
-            const std::string url = argv[1];
-            std::cout << url << std::endl;
-        }
+        const std::string url = argv[1];
+        std::cout << url << std::endl;
     }
 }

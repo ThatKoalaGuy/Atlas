1e13384b495b5e83f67d816226d7497480f488be
Niko Rutar <nikor5200@gmail.com>
2026-01-16 17:54:50 +0100

    feat: added help and version menu

---
+++ include/atlas/command/help.h
@@ -0,0 +1,10 @@
+#ifndef ATLAS_HELP_H
+#define ATLAS_HELP_H
+
+
+namespace atlas {
+    void help();
+}
+
+
+#endif //ATLAS_HELP_H
--- src/parser.cpp
+++ src/parser.cpp
@@ -1,11 +1,25 @@
 #include <iostream>
 #include <ostream>
 #include <atlas/parser.h>
+#include <atlas/command/help.h>
 
 namespace atlas {
     //argc will be used later, do not delete!
     void parser(const int argc, char *argv[]) {
-        const std::string url = argv[1];
-        std::cout << url << std::endl;
+        if (argc != 2) {
+            std::cout <<"Invalid usage, write atlas --help to help."<< std::endl;
+        }
+        else {
+            if (argv[1] == "--help"|| argv[1] == "-h") {
+                atlas::help();
+            }
+            else if (argv[1] == "--version" || argv[1] == "-v") {
+                std::cout << "Current version: \n"
+                "v1.0.0-alpha";
+            }
+        }
+
+
+
     }
 }
